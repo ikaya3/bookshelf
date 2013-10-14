be320c2b93af1370efa42d9c81a196b83d233a95
Itagaki Kohta <ikaya3@gmail.com>
2013-10-14 09:56:11 +0900

    product に対する制約の追加、およびそれに対するテスト

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than: 0}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)$}i,
+    message: 'はGIF/JPG/PNGのいずれかである必要があります。'
+  }
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attribute must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "hoge",
+                          description: "YYY",
+                          image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than 0",
+    product.errors[:price].join("; ")
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than 0",
+    product.errors[:price].join("; ")
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "hogehoge",
+                description: "fugafuga",
+                price: 23.45,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.jpg
+             http://x.y.z/a/b/c/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?
+    end
+  end
+
+  test "use fixture" do
+    assert products(:one).valid?
+    assert products(:two).invalid?
+  end
 end
