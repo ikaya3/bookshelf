e9d6fe0f0662998b3634b8594ff5264587ec13b2
Itagaki Kohta <ikaya3@gmail.com>
2013-10-14 21:25:30 +0900

    機能テストの追加

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,9 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side li', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
   end
 
 end
