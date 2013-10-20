f057856f40e3848d815ceed352f38dae80980d5c
Itagaki Kohta <ikaya3@gmail.com>
2013-10-20 14:28:41 +0900

    カートの作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than: 0}
   validates :title, uniqueness: true
@@ -8,4 +11,16 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'はGIF/JPG/PNGのいずれかである必要があります。'
   }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'item is existed.')
+      return false
+    end
+  end
+
 end
